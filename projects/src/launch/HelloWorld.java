13ba5f865ad6e0a8a8068b3bcb0b7535ff62dde8
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2020-09-24 08:44:36 -0700

    Finished IDE setup lab

---
+++ projects/src/launch/HelloWorld.java
@@ -0,0 +1,8 @@
+package launch;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Ryan Hendrickson");
+        System.out.println("I have used the following IDEs: IntelliJ, Eclipse, CLion, and PHPStorm.");
+    }
+}
